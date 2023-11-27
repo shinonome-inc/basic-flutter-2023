9f36011b87b4fda5f33318fe55630f3efeb0502b
yousei527 <y.nakanishi.23.10.1196@4nonome.com>
2023-11-27 21:57:41 +0900

    commit message

--- leap_year_algorithm/leap_year_algorithm.dart
+++ leap_year_algorithm/leap_year_algorithm.dart
@@ -1,3 +1,19 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+import 'dart:io';
+
+void main() {
+  print('整数を入力してください。');
+  String input = stdin.readLineSync() ?? ''; // 標準入力から文字列を読み込む
+  int number = int.parse(input); // 文字列を整数に変換
+  isLeapYear(year: number);
+}
+
+class isLeapYear{
+  int year = 0;
+  isLeapYear({year}) {
+    if(year%4 == 0) {
+      if (year % 400 == 0 || year % 100 != 0) {
+        print(year);
+      }
+    }
+  }
 }
